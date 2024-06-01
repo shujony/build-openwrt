e1e61d5a4ae7ac1cd57553ecb24ab340bebe8689
shujony <shujony@gmail.com>
2024-06-01 17:20:17 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -8,6 +8,9 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git clash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns.git' feeds.conf.default
+sed -i '$a src-git zerotier https://github.com/zerotier/ZeroTierOne.git' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
